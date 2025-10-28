692ae1c80999bc3d57b845d0db91b6797753a2e2
Nigoat <ferr3iran@gmail.com>
2025-10-28 11:13:14 +0100

    Update MainMVC.java

--- AP_2.0/src/Controller/MainMVC.java
+++ AP_2.0/src/Controller/MainMVC.java
@@ -1,21 +1,25 @@
 package Controller;
 
-import java.sql.SQLException;
+import javax.swing.SwingUtilities;
 import View.View_Accueil;
 import Model.model;
+import java.sql.SQLException;
 
 public class MainMVC {
-	private static model m;
- 
-	public static model getM() {
-	 return m;
- }
- 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		System.out.println("lancement de mon programme");
-		m=new model(); 
-		View_Accueil va = new View_Accueil();
+    private static model m;
 
+    public static model getM() {
+        return m;
+    }
 
-	}
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        System.out.println("lancement de mon programme");
+        m = new model();
+
+        // Create and show the view on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            View_Accueil va = new View_Accueil();
+            va.setVisible(true);
+        });
+    }
 }
